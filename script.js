8e80c5d77fc50738d7f90e308f2ca193637287ac
daniilantonov456-maker <daniilantonov456@gmail.com>
2026-01-03 00:59:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let tg = window.Telegram.WebApp;
 
+// Массив для корзины
+let cart = [];
+
+// Функция добавления товара в корзину
 function buy(product) {
-    tg.sendData(product);
+    cart.push(product);
+    alert(`${product} добавлен в корзину!`);
+}
+
+// Функция отправки корзины в бота
+function sendCart() {
+    if(cart.length === 0){
+        alert("Корзина пуста!");
+        return;
+    }
+    // Отправляем все товары через WebApp
+    tg.sendData(JSON.stringify(cart));
+    cart = []; // очищаем корзину после отправки
 }
